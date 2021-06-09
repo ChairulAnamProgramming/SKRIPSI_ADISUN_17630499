34b0c63e2ae2ceb6fe0f0d2264569b240f075af2
Chairul Anam <chairulanamart16@gmail.com>
2021-06-09 11:01:37 +0700

    Version 1.0.0

--- resources/views/pages/Stok-Pangan/show.blade.php
+++ resources/views/pages/Stok-Pangan/show.blade.php
@@ -3,9 +3,9 @@
 @section('content')
 
 <div class="container">
-    <div class="card text-white bg-primary">
+    <div class="card">
         <img class="card-img-top" src="{{url('public/storage').'/'.$foodItem->image}}" alt="{{$foodItem->title}}"
-            class="img-fluid">
+            class="img-fluid" style="height: 400px">
         <div class="card-body">
             <h4 class="card-title">Title</h4>
             <p class="card-text">Text</p>
